9e4b83d58d0d292e7433a00a3dd56ae24a481015
lhourquin <lhourquin@gmail.com>
2025-04-12 03:08:55 +0200

    refactor: remove AppController, AppService, and configuration files; introduce RootModule for improved structure

--- apps/server/src/root.module.ts
+++ apps/server/src/root.module.ts
@@ -1,28 +1,27 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { CqrsModule } from '@nestjs/cqrs';
 import { SuperTokensAuthGuard } from 'supertokens-nestjs';
 import { AuthModule } from '@infrastructures/auth/auth.module';
-import { CqrsModule } from '@nestjs/cqrs';
 import { CqrsWiringModule } from '@infrastructures/cqrs/cqrs-wiring.module';
+import { PresentationModule } from '@presentation/presentation.module';
 @Module({
   imports: [
-    CqrsModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    CqrsModule.forRoot(),
     AuthModule,
     CqrsWiringModule,
+    PresentationModule,
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
-    AppService,
     {
       provide: APP_GUARD,
       useClass: SuperTokensAuthGuard,
     },
   ],
 })
-export class AppModule {}
+export class RootModule {}
